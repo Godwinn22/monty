73f639dc22eb4e4954eae86a8c06b8079ecd3416
godwinn22 <godwinohazulike@gmail.com>
2024-01-18 20:36:48 +0100

    Wrote a function that sets the format of the data to a stack (LIFO)

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * m_stack - sets the format of the data to a stack (LIFO)
+ * @head: double pointer to the stack_t node
+ * @line: line number of the opcode in the file
+ */
+void m_stack(stack_t **head, unsigned int line)
+{
+	(void)head;
+	(void)line;
+	transpInfo.trficIdx = 0;
+}
