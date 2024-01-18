bf308b5ac60193ab07d4e2f81d216ee1c010d6ce
godwinn22 <godwinohazulike@gmail.com>
2024-01-18 17:37:34 +0100

    A function that Does nothing.

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * m_nop - Does nothing.
+ * @head: Pointer to the head of the stack.
+ * @line: Line number of the Monty bytecodes file.
+ */
+void m_nop(stack_t **head, unsigned int line)
+{
+	(void)line;
+	(void)head;
+}
