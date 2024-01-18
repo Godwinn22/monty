02aedc3acb017740edcbf01499c1c8118e52eb65
godwinn22 <godwinohazulike@gmail.com>
2024-01-18 17:38:41 +0100

    Wrote a function that sets the mode to queue

---
+++ f_queue.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_queue - sets the mode to queue
+ * @head: pointer to the head of the stack
+ * @line: line number
+ */
+void f_queue(stack_t **head, unsigned int line)
+{
+	(void)head;
+	(void)line;
+	transpInfo.trficIdx = 1;
+}
